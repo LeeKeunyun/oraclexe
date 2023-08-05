15bc57752c15836974f03a39ac39a54b02c985aa
LeeKeunyun <muscle1980@naver.com>
2023-08-05 15:50:44 +0900

    Update ex05-function.sql

--- ex05-function.sql
+++ ex05-function.sql
@@ -65,3 +65,107 @@ select REPLACE('JACK and JUE', 'J', 'BL') FROM dual; -- J 문자를 BL 문자로 바꿈
 */
 select TRIM('H' FROM 'HelloWorld') FROM dual;
 select TRIM(' ' FROM ' HelloWorld ') FROM dual;
+
+
+/*
+  13. 숫자 함수
+  ROUND() : 소숫자 [2번째 파라미터 입력수치]번째 자리수 까지 반올림
+  TRUNC() : 소숫자 [2번째 파라미터 입력수치]번째 자리수 이하는 절삭
+  CELL()  : 주어진 숫자를 올림하여 정수로 반환
+  MOD()   : 나눈 나머지를 반환
+*/
+SELECT ROUND(45.926, 2) FROM dual; -- 45.93
+SELECT TRUNC(45.926, 2) FROM dual; -- 45.92
+SELECT CEIL(45.111) FROM dual; -- 46
+SELECT MOD(1600, 300) FROM dual; -- 100
+
+
+/*
+  14. 날짜 함수
+  
+  SYSDATE 를 조회시 05-08-23 로 표시 (일-월-년 기본값)
+  
+
+  그런데 SQL developer 에서는 환경설정 (데이터베이스 > NLS > 날짜형식(F)) 이 
+   : DD-MM-RR 로 설정되어 있을 경우 결과는 05-08-23 로 나옴
+   : RR/MM/DD 로 설정되어 있을 경우 결과는 23/08/05 로 나옴  
+*/
+
+SELECT SYSDATE FROM dual;
+
+
+-- 15. 날짜 함수는 연산이 가능
+
+select last_name, (sysdate - hire_date) / 7 as weeks
+from employees
+where department_id = 90;
+
+
+/*
+  16. 날짜 조작 함수
+
+  MONTHS_BETWEEN(date1, date2) : 두 날짜 간의 월 수를 계산
+  ADD_MONTHS(date, n) : 날짜에 n개월을 추가
+  NEXT_DAY(date, day_of_week) : 지정된 날짜의 다음으로 주어진 요일이 나오는 날짜를 계산
+                                (1:월요일 ~ 7:일요일)
+  LAST_DAY(date) : 주어진 월의 마지막 날짜를 반환
+  ROUND(date, format) : 날짜를 지정된 형식으로 반올림
+  TRUNC(date, format) : 날짜를 지정된 형식으로 절삭
+*/
+
+select MONTHS_BETWEEN(TO_DATE('2016-12-22', 'YYYY-MM-DD'), TO_DATE('2017-05-22', 'YYYY-MM-DD')) AS "결과"
+from dual;
+-- 결과는 -5 로 표시
+
+select MONTHS_BETWEEN(TO_DATE('2017-12-22', 'YYYY-MM-DD'), TO_DATE('2017-05-22', 'YYYY-MM-DD')) AS "결과"
+from dual;
+-- 결과는 7 로 표시
+
+select ADD_MONTHS(TO_DATE('2022-12-16', 'YYYY-MM-DD'), 1) AS "결과"
+from dual;
+-- 결과 : 16-01-23
+
+select NEXT_DAY(TO_DATE('2023-08-05', 'YYYY-MM-DD'), 7) AS "결과"
+from dual;
+-- 결과 : 12-08-23
+
+select LAST_DAY(TO_DATE('2023-08-05', 'YYYY-MM-DD')) AS "결과" from dual;
+select LAST_DAY(SYSDATE) AS "결과" from dual;
+-- 결과 : 31-08-23
+
+select ROUND(TO_DATE('2023-08-02', 'YYYY-MM-DD'), 'month') AS "결과" from dual;
+-- 결과 : 01-08-23
+
+select ROUND(TO_DATE('2023-08-26', 'YYYY-MM-DD'), 'month') AS "결과" from dual;
+-- 결과 : 01-09-23
+
+select TRUNC(TO_DATE('2023-08-02', 'YYYY-MM-DD'), 'month') AS "결과" from dual;
+-- 결과 : 01-08-23
+
+select TRUNC(TO_DATE('2023-08-26', 'YYYY-MM-DD'), 'month') AS "결과" from dual;
+-- 결과 : 01-08-23
+
+
+
+/*
+17. 변환 함수
+ 
+TO_CHAR() : 날짜 또는 숫자를 문자열로 변환
+ YYYY : 전체 연도를 숫자로 표시.
+ YEAR : 영어 철자로 표기된 연도를 반환.
+ MM : 월의 2자리 숫자 값
+ MONTH : 전체 월 이름
+ MON : 월의 3자리 약어
+ DY : 3자리 요일 약어
+ DAY : 요일의 전체 이름
+ DD : 월간 일(1-31)의 숫자 형식
+ HH : 1일 동안의 시간
+ HH12 : 반일 시간(1-12)
+ HH24 : 전일 시간(0-23)
+ MI : 분(0-59)
+ SS : 초(0-59)
+ FF : 밀리세컨드(0-999)
+ AM 또는 PM : 오전 또는 오후를 나타내는 자오선 표시를 반환
+ A.M. 또는 P.M. : 오전 또는 오후를 나타내는 마침표가 있는 자오선 표시를 반환
+ 
+*/
